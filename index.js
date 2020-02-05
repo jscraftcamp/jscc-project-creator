27b44201f84d5450eefb1554b988cbbba4fa11cf
David Losert <david.losert@gmx.de>
2020-02-05 17:21:19 +0100

    Added script to read current issues and write them to a json file

--- index.js
+++ index.js
@@ -5,25 +5,36 @@ const util = require('util');
 const readFile = util.promisify(fs.readFile);
 
 async function main() {
-  const endpoint = 'https://api.github.com/graphql'
-  const token = await readFile('./.github_token');
+    const endpoint = 'https://api.github.com/graphql'
+    const token = await readFile('./.github_token');
 
-  const graphQLClient = new GraphQLClient.GraphQLClient(endpoint, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  })
+    const graphQLClient = new GraphQLClient.GraphQLClient(endpoint, {
+        headers: {
+            Authorization: `bearer ${token}`,
+        },
+    })
 
-  const query = `{
-        viewer {
-            login
-            name
+    const query = `{
+        repository(owner:"jscraftcamp", name:"website") {
+      
+          issues(labels:["jscc20"], first:40) {
+          nodes {
+            title,
+            body
+          }
+        }
+          
+          
         }
     }
   `
 
-  const data = await graphQLClient.request(query)
-  console.log(JSON.stringify(data, undefined, 2))
+    const data = await graphQLClient.request(query);
+
+    const issues_2020 = JSON.stringify(data.repository.issues.nodes);
+    fs.writeFileSync('./input/issues.json', issues_2020);
+
+    console.log(JSON.stringify(data, undefined, 2))
 }
 
 main().catch(error => console.error(error))
